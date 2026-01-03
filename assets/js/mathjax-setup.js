ae84785c9a6f2bc86114a6309c3c1a00afa7603d
Dominic Savarino <136136566+domsav13@users.noreply.github.com>
2026-01-02 22:01:18 -0500

    Update mathjax-setup.js

--- assets/js/mathjax-setup.js
+++ assets/js/mathjax-setup.js
@@ -13,10 +13,13 @@ window.MathJax = {
         function (doc) {
           const style = document.createElement("style");
           style.innerHTML = `
-          .mjx-container {
-            color: inherit;
+          .mjx-container { color: inherit; }
+
+          mjx-container[jax="CHTML"][display="true"] {
+            overflow-x: hidden !important;
+            overflow-y: hidden !important;
           }
-        `;
+          `;
           document.head.appendChild(style);
         },
         "",
