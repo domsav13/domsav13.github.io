57b994adaa537c48b191e9e7189c58ccdbb087ed
domsav13 <136136566+domsav13@users.noreply.github.com>
2026-01-25 05:33:00 +0000

    Deploying to gh-pages from @ domsav13/domsav13.github.io@0ff5dd8364a9a7d45e4ad95edfeeffb6f8a8a051 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -194,32 +194,7 @@ ninja.data = [{
           section: "News",},{id: "news-first-day-as-graduate-teaching-assistant-for-the-cad-lab-mae-1004-at-gw-️-gear",
           title: 'First day as Graduate Teaching Assistant for the CAD Lab (MAE 1004) at...',
           description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
+          section: "News",},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
